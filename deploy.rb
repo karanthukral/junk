a67ff43b61e7acb10b08422479ec943e66933b85
Karan Thukral <karanthukral@gmail.com>
2021-03-03 11:27:48 -0500

    print more links

--- deploy.rb
+++ deploy.rb
@@ -3,5 +3,10 @@
 splunk = "https://logs.shopify.io/en-US/app/search/search?q=search+sourcetype%3Dfluentd+kube_cluster%3Dapps-b-us-central1-3+kube_namespace%3Dgkrane-jobs+kube_container%3Dgkrane+kube_pod%3Dmobility-test-app-production-unrestricted-8tg6k%2A+earliest%3D1614711248+%7C+table+log%2Cmsg"
 html = "https://storage.cloud.google.com/gkrane-logs/apps-b-us-central1-3/mobility-test-app-production-unrestricted/2bb04b9bcae81f55535b757b8c89d39e.html"
 
+splunk2 = "https://logs.shopify.io/en-US/app/search/search?q=search+sourcetype%3Dfluentd+kube_cluster%3Dapps-b-us-central1-2+kube_namespace%3Dgkrane-jobs+kube_container%3Dgkrane+kube_pod%3Dmobility-test-app-production-unrestricted-8tg6k%2A+earliest%3D1614711248+%7C+table+log%2Cmsg"
+html2 = "https://storage.cloud.google.com/gkrane-logs/apps-b-us-central1-2/mobility-test-app-production-unrestricted/2bb04b9bcae81f55535b757b8c89d39e.html"
+
 puts "Splunk Logs = #{splunk} | HTML Logs = #{html}"
+puts "Splunk Logs = #{splunk} | HTML Logs = #{html}"
+puts "Splunk Logs = #{splunk2} | HTML Logs = #{html2}"
 puts "https://buildkite.com/shopify/bespin-multi-deploy"
